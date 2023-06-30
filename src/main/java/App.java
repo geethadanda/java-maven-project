0bafbf9dba1ab9998458d50354426a5a323f0f04
geethadanda <35402967+geethadanda@users.noreply.github.com>
2023-06-30 11:29:33 +0000

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,6 @@ public class App
 
         DeepThought ourSuperComputer = new DeepThought();
         int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
-        System.out.println(ans);
+        System.out.println("Here is your answer" +ans);
     }
 }
